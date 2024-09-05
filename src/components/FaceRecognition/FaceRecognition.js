08d1f9bbe94e0406966f22ec89f905f5ba481278
elpatrico11 <elpatrico21@gmail.com>
2024-09-05 08:39:52 +0200

    binded input to an actual image so it changes, when I submit on a button

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const FaceRecognition = () => {
+const FaceRecognition = ({imageUrl}) => {
   return (
     <div className='center'>
-        <img src = {'https://samples.clarifai.com/metro-north.jpg'} alt='' />
+        <img src = {imageUrl} alt='' />
     </div>
   );
 }
